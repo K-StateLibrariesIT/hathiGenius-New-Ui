3acec17e31c2cb724cfba2a49f50a82cc9c395d3
Tom Misilo <1446856+misilot@users.noreply.github.com>
2020-02-25 15:53:05 -0600

    Updated HathiGenius code with what is in prod

--- custom_js_for_package.js
+++ custom_js_for_package.js
@@ -2,106 +2,108 @@
     "use strict";
     'use strict';
 
-  app.component('prmSearchResultAvailabilityLineAfter', {
-        
+    app.component('prmSearchResultAvailabilityLineAfter', {
         bindings: { parentCtrl: '<' },
         controller: 'prmSearchResultAvailabilityLineAfterController'
     });
 
-app.controller('prmSearchResultAvailabilityLineAfterController', function($scope) {
+    app.controller('prmSearchResultAvailabilityLineAfterController', function ($scope) {
 
-//Bengtson-Fu 13 3 |\| ( ][ 5 () |\| - |= |_|!!!!!!
- 
-/*created by Jason Bengtson, MLIS, MA for K-State Libraries ITS: Available under GNU GPL license*/
- 
-/*Bengtson-Fu is the best Kung Fu!!!*/
- 
-/*Built for Kansas State University -- Go Wildcats!!!*/
-/*hathiGenius: NewUI-----an app built to determine availability of hathitrust materials*/
+    /* Begin Hathi Genius */
+    /* Bengtson-Fu 13 3 |\| ( ][ 5 () |\| - |= |_|!!!!!! */
+    /*created by Jason Bengtson, MLIS, MA for K-State Libraries ITS: Available under GNU GPL license*/
+    /* Bengtson-Fu is the best Kung Fu!!! */
+    /* Built for Kansas State University -- Go Wildcats!!! */
+    /* hathiGenius-----an app built to determine availability of hathitrust materials */
 
-
-function rewrite(datareturn) {
-    $.each( datareturn, function( i, val ) {
-        console.log(val);
-        var datext="Not Available Online";
-		var daclass="info";
-        if(val.article.indexOf("StatusAvailable")>0) {
-            datext="Available Online";
-			daclass="fulltext_linktorsrc";
+        function rewrite(datareturn)
+        {
+            $.each(datareturn, function ( i, val ) {
+                var datext="Not Available Online";
+                var daclass="info";
+                if (val.article.indexOf("StatusAvailable")>0) {
+                    datext="Available Online";
+                    daclass="fulltext_linktorsrc";
+                }
+                $("#"+val.ident).find("span.button-content>span").attr("class","").addClass(val.article).addClass(daclass).text(datext);
+            });
         }
-        $("#"+val.ident).find("span.button-content>span").attr("class","").addClass(val.article).addClass(daclass).text(datext);
-    });
-}
- 
 
-function actionIt(datareturn) {
-	datareturn=JSON.parse(datareturn);
-                    rewrite( datareturn );
-}
-function hathiFix() {     
+        function rewriteFailure(datareturn)
+        {
+            $.each(datareturn, function ( i, val ) {
+                var datext="Check Availability";
+                var daclass="info";
+                if (val.article.indexOf("StatusAvailable")>0) {
+                    datext="Available Online";
+                    daclass="fulltext_linktorsrc";
+                }
+                $("#"+val.ident).find("span.button-content>span").attr("class","").addClass(val.article).addClass(daclass).text(datext);
+            });
+        }
+
+        function hathiFix()
+        {
             var datasink=new Array();
             var x=0;
-            $("div.list-item-primary-content").each(function(i, obj) {
+            $("div.list-item-primary-content").each(function (i, obj) {
                 var linko=$(obj).find("span.urlToXmlPnx").attr("data-url")+"";
-                
-                  
-                if(linko.indexOf("TN_hathi_trust")>-1) {
-					$(obj).find("span.availability-status.fulltext_linktorsrc").text("Checking Holdings . . .");
+
+                if (linko.indexOf("TN_hathi_trust")>-1) {
+                    $(obj).find("span.availability-status.fulltext_linktorsrc").text("Checking Holdings . . .");
                     var tooler=Math.floor((Math.random() * 99000) + 10000);
                     $(obj).attr("id","newid"+tooler);
-                    
-                    
+
                     datasink[x]=new Object();
-                    var uriarray = linko.split( '/' );
+                    var uriarray = linko.split('/');
                     var sizeit=uriarray.length-1;
                     var disuri=uriarray[sizeit];
                     var isit=disuri.indexOf("?");
-                    if(isit>-1) {
+                    if (isit>-1) {
                         disuri=disuri.substring(0,isit);
-         
                     }
-					
+
                     datasink[x].ident=$(obj).attr("id");
                     datasink[x].article=disuri.replace("TN_hathi_trust", "");
-                    
-                     
+
                     x++;
                 }
-                 
+
             });
 
-             
             var sendo=JSON.stringify(datasink);
-              
-        $.getScript("PATH TO HATHILOOKUP.PHP"+sendo, function() {
-			
-                   setTimeout(actionIt(sendo), 100); 
+             $.ajax({
+                    dataType: "json",
+                    url: "https://PATH TO HATHILOOKUP.PHP?datasink="+sendo,
+                    success: function (data) {
+                        rewrite(data);
+                    },
+                    error: function (xhr, status, error) {
+                        rewriteFailure(datasink);
+                    }
                 });
-}
+        }
 
+        // Setup a new observer to get notified of changes
+        var observer = new MutationObserver(function (mutations) {
+            mutations.forEach(function (mutation) {
+                $(mutation.target).find('div.last-item').text(function (_, txt) {
+                    hathiFix();
+                })
+            });
+        });
 
-// Setup a new observer to get notified of changes
-var observer = new MutationObserver(function (mutations) {
-  mutations.forEach(function(mutation) {
-    $(mutation.target).find('div.last-item').text(function (_, txt) {
-      hathiFix();
+        // Observe a specific DOM node / subtree
+        observer.observe($('.main')[0], {
+            childList: true
+        });
+
+        $(".list-item-wrapper").click(function () {
+            setTimeout(hathiFix(), 800);
+        });
+
+        hathiFix();
+        /* End HathiGenius */
+
     })
-  });
 });
-
-// Observe a specific DOM node / subtree
-observer.observe($('.main')[0], {
-  childList: true
-});
-
-$(".list-item-wrapper").click( function() {
-	setTimeout(hathiFix(), 800);
-});
-
-
-hathiFix();
-
-
-
-})
-}();
--- hathilookup.php
+++ hathilookup.php
@@ -1,58 +1,37 @@
 <?php
-/*Bengtson-Fu 13 3 |\| ( ][ 5 () |\| - |= |_|!!!!!!*/
+/* Bengtson-Fu 13 3 |\| ( ][ 5 () |\| - |= |_|!!!!!! */
+/* Created by Jason Bengtson, MLIS, MA for K-State Libraries ITS */
+/* Available under Creative Commons non-commercial share alike open source license */
+/* Bengtson-Fu is the best Kung Fu!!!*/
+/* Built for Kansas State University -- Go Wildcats!!! */
+/* hathiGenius-----an app built to determine availability of hathitrust materials */
 
-/*created by Jason Bengtson, MLIS, MA for K-State Libraries ITS : Available under Creative Commons non-commercial share alike open source license*/
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
 
-/*Bengtson-Fu is the best Kung Fu!!!*/
+$datasink=$_GET["datasink"];
+$checko=strlen($datasink);
+$datasink=json_decode($datasink);
+$reverso=$_SERVER['HTTP_REFERER'];
+$test1=strpos($reverso, "tab=");
+$test2=strpos($reverso, "vid=");
 
-/*Built for Kansas State University -- Go Wildcats!!!*/
-/*hathiGenius-----an app built to determine availability of hathitrust materials*/
-if (!ini_get('log_startup_errors')) {
-    ini_set('log_startup_errors', '1');
+if (($checko>3) && ($test1>0) && ($test2>0)) {
+    foreach ($datasink as &$value) {
+        $query="https://catalog.hathitrust.org/api/volumes/brief/htid/".$value->article.".json";
+
+        $thisjson = file_get_contents($query);
+        $thisjson=json_decode($thisjson);
+        $code2="EXLResultStatusNotAvailable";
+        foreach ($thisjson->items as &$jason) {
+            if ($jason->htid===$value->article) {
+                $code=$jason->usRightsString;
+                if ($code==="Full view") {
+                        $code2="EXLResultStatusAvailable";
+                }
+            }
+        }
+        $value->article=$code2;
+    }
+    echo json_encode($datasink);
 }
-
-if (!ini_get('log_errors')) {
-    ini_set('log_errors', true);
-}
-
-
-// Set Path to your error log folder
-$path = null;
-
-if (!ini_get('error_log') && !is_null($path)) {
-    ini_set("error_log", $path . "hathilookup.log");
-}
-
-	$datasink=$_GET["datasink"];
-	$checko=strlen($datasink);
-	$datasink=json_decode($datasink);
-	$reverso=$_SERVER['HTTP_REFERER'];
-	$test1=strpos($reverso, "tab=");
-	$test2=strpos($reverso, "vid=");
-	if(($checko>3) && ($test1>0) && ($test2>0)) {
-	
-	foreach ($datasink as &$value) {
-		$query="https://catalog.hathitrust.org/api/volumes/brief/htid/".$value->article.".json";
-
-		$thisjson = file_get_contents($query);
-		$thisjson=json_decode($thisjson);
-		$code2="EXLResultStatusNotAvailable";
-		foreach ($thisjson->items as &$jason) {
-			if($jason->htid===$value->article) {
-				$code=$jason->usRightsString;
-				
-				if($code==="Full view") {
-						$code2="EXLResultStatusAvailable";	
-				}
-				
-			}
-		}
-		
-		$value->article=$code2;
-		
-	}
-	
-	echo("var datareturn='".json_encode($datasink)."';");
-
-}
-	
